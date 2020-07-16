3a90f609a4071a90396bf50881ee5a69b689455c
Raahul Vignesh <raahulvignesh@Raahuls-MacBook-Pro.local>
2020-07-16 14:33:57 +0530

    added extra loggers

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "1.0"
 scalaVersion := "2.12.10"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
+libraryDependencies += "org.wvlet.airframe" %% "airframe-log" % "20.6.2"
 libraryDependencies += "com.ibm.stocator" % "stocator" % "1.0.35"
 libraryDependencies += "com.ibm.db2.jcc" % "db2jcc" % "db2jcc4"
--- src/main/scala/IbmExercise.scala
+++ src/main/scala/IbmExercise.scala
@@ -1,19 +1,20 @@
 package com.ibm.stocator;
 
+import wvlet.log.LogSupport
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions.regexp_replace
 
-object IbmExercise {
+object IbmExercise extends LogSupport{
 
 	// Place your COS Credentials
-	val access_key = ""
-	val secret_key = ""
-	val endpoint = ""
+	val access_key : String = ""
+	val secret_key : String = ""
+	val endpoint : String = ""
 
 	// Place your DB2 Credentials
-	val url = ""
-	val username = ""
-	val password = ""
+	val url : String = ""
+	val username : String = ""
+	val password : String = ""
 
 	def main(args: Array[String]) {
 		
@@ -23,52 +24,70 @@ object IbmExercise {
 		// reads data from Cos and stores it in dataframe
 		var dataframe = readDataFromCos(spark)
 		// writes the dataframe into DB2 using JBLC driver
-		val written_df = DB2_Operations("write",spark, dataframe, "jdbc", "FFL38638.EMPLOYEE_DATA", url, username, password)
+		val written_df : DataFrame = DB2_Operations("write",spark, dataframe, "jdbc", "FFL38638.EMPLOYEE_DATA", url, username, password)
 		// reads the data from DB2 and loads it into dataframe
 		var read_dataframe = DB2_Operations("read",spark, null, "jdbc", "FFL38638.EMPLOYEE_DATA", url, username, password)
 		// performs the computation operation and stores the result into COS as parquet
 		calculateResult(spark, read_dataframe)
 		// reads the stored parquet files
 		spark.read.parquet("cos://candidate-exercise.myCos/emp-data.csv/result0.parquet").show()
+		info("Main : " + "result0.parquet read from COS")
 		spark.read.parquet("cos://candidate-exercise.myCos/emp-data.csv/result1.parquet").show()
+		info("Main : " + "result1.parquet read from COS")
 		spark.stop()
 	}
 
+	// Function to read the data from COS
 	def readDataFromCos(spark: SparkSession): DataFrame = {
 	
 		// Reading the emp-data.csv into the dataframe
-		val csvPath = "cos://candidate-exercise.myCos/emp-data.csv"
-		val df = spark.read.format("csv")
-		.option("header", "true")
-		.option("inferSchema", "true")
-		.load(csvPath)
-		return df
+		val csvPath : String  = "cos://candidate-exercise.myCos/emp-data.csv"
+		try {
+			val df : DataFrame = spark.read.format("csv")
+				.option("header", "true")
+				.option("inferSchema", "true")
+				.load(csvPath)
+			info("readDataFromCos : " + "Read file data " + csvPath + " from COS")
+			return df
+		} catch {
+			case e : Throwable => error("readDataFromCos : " + "Error While reading file path " + csvPath, e)
+			return spark.emptyDataFrame
+		}
+		
 	}
 
-	def writeDataToCos(spark: SparkSession, dataframe: DataFrame, filePath: String): DataFrame = {
+	// Function to write data from COS
+	def writeDataToCos(dataframe: DataFrame, filePath: String) {
 
-	    //connecting to IBM COS
-	    var spark_temp = connectionToCos(spark)
-	    // Storing the data into IBM COS
-	    dataframe.write
-			.mode("overwrite")
-			.parquet(filePath)
-		return dataframe
+		// Storing the data into IBM COS
+		try {
+			dataframe.write
+				.mode("overwrite")
+				.parquet(filePath)
+			info("writeDataToCos : " + "Written Data to " + filePath)
+		} catch {
+			case e : Throwable => error("writeDataToCos : " + "Error While writing to file path " + filePath + " from COS", e)
+		}
 	}
 
 	// Function makes connection to IBM cos
 	def connectionToCos(spark: SparkSession) {
 	
-		val prefix = "fs.cos.myCos"
+		val prefix : String = "fs.cos.myCos"
 		// configuring the stocator using HMAC method
-		spark.sparkContext.hadoopConfiguration.set("fs.cos.impl", "com.ibm.stocator.fs.ObjectStoreFileSystem")
-		spark.sparkContext.hadoopConfiguration.set("fs.stocator.scheme.list", "cos")
-		spark.sparkContext.hadoopConfiguration.set("fs.stocator.cos.impl", "com.ibm.stocator.fs.cos.COSAPIClient")
-		spark.sparkContext.hadoopConfiguration.set("fs.stocator.cos.scheme", "cos")
-		spark.sparkContext.hadoopConfiguration.set(prefix + ".access.key",access_key)
-		spark.sparkContext.hadoopConfiguration.set(prefix + ".secret.key",secret_key)
-		spark.sparkContext.hadoopConfiguration.set(prefix + ".endpoint",endpoint)
-		spark.sparkContext.hadoopConfiguration.set(prefix + ".v2.signer.type", "false")
+		try {
+			spark.sparkContext.hadoopConfiguration.set("fs.cos.impl", "com.ibm.stocator.fs.ObjectStoreFileSystem")
+			spark.sparkContext.hadoopConfiguration.set("fs.stocator.scheme.list", "cos")
+			spark.sparkContext.hadoopConfiguration.set("fs.stocator.cos.impl", "com.ibm.stocator.fs.cos.COSAPIClient")
+			spark.sparkContext.hadoopConfiguration.set("fs.stocator.cos.scheme", "cos")
+			spark.sparkContext.hadoopConfiguration.set(prefix + ".access.key",access_key)
+			spark.sparkContext.hadoopConfiguration.set(prefix + ".secret.key",secret_key)
+			spark.sparkContext.hadoopConfiguration.set(prefix + ".endpoint",endpoint)
+			spark.sparkContext.hadoopConfiguration.set(prefix + ".v2.signer.type", "false")
+			info("connectionToCos : " + "Connection to COS established!")
+		} catch {
+			case e  : Throwable => error("connectionToCos : " + "Error while establishing connection to COS", e)
+		}
 	}
 
 	// Function to calculate query results
@@ -77,20 +96,35 @@ object IbmExercise {
 		// creating a temporary table for computation
 		dataframe.registerTempTable("employees")
 		// query to calcualte the gender ratio (Male/Female) of each department
-		val genderRatio = spark.sql("Select Department, sum(case when gender='Male' then 1 else 0 end)/sum(case when gender='Female' then 1 else 0 end) as GenderRatio from employees group by Department")
+		val query_1 : String = """Select Department, 
+									sum(case when gender='Male' then 1 else 0 end)/
+									sum(case when gender='Female' then 1 else 0 end) as GenderRatio 
+									from employees group by Department"""
+		try {
+			val genderRatio : DataFrame = spark.sql(query_1)
+			info("calcualteResult : " + "Query 1 Processed")
+			val filePath1 : String = "cos://candidate-exercise.myCos/emp-data.csv/result0.parquet"
+			writeDataToCos(genderRatio, filePath1)
+		} catch {
+			case e  : Throwable => error("calcualteResult : " + "Error while processing Query 1", e)
+		}
 		// creating a temporary table with salary converted into float for performing math operations
 		var dataframe_cleaned = spark.sql("Select Department, Gender, substring(Salary, 2) as Salary from employees")
 		dataframe_cleaned = dataframe_cleaned.withColumn("Salary", regexp_replace(dataframe_cleaned("Salary"), "\\,", "").cast("float"))
 		dataframe_cleaned.registerTempTable("employees2")
 		// computes the average salary and salary gap(male - female) of each department
-		val averageSalary = spark.sql("Select Department, avg(Salary) as Salary_avg, avg(case when Gender='Male' then Salary end) - avg(case when Gender='Female' then Salary end) as Salary_gap from employees2 group by Department")
-		genderRatio.show()
-		averageSalary.show()
+		val query_2 : String = """ Select Department, avg(Salary) as Salary_avg, 
+									avg(case when Gender='Male' then Salary end) - avg(case when Gender='Female' 
+									then Salary end) as Salary_gap from employees2 group by Department"""
+		try {
+			val averageSalary : DataFrame = spark.sql(query_2)
+			info("calcualteResult : " + "Query 2 Processed")
+			val filePath2 : String = "cos://candidate-exercise.myCos/emp-data.csv/result1.parquet"
+			writeDataToCos(averageSalary, filePath2)
+		} catch {
+			case e  : Throwable => error("calcualteResult : " + "Error while processing Query 2", e)
+		}
 		// writing the result into IBM COS as parquet file
-		val filePath1 = "cos://candidate-exercise.myCos/emp-data.csv/result0.parquet"
-		val filePath2 = "cos://candidate-exercise.myCos/emp-data.csv/result1.parquet"
-		writeDataToCos(spark, genderRatio, filePath1)
-		writeDataToCos(spark, averageSalary, filePath2)
 	}
 
 	// Function to perform operation on DB2
@@ -100,24 +134,37 @@ object IbmExercise {
 		// match case to perform read or write operation
 		operation.trim() match {
 			case "read" => {
-				val jdbcDF = spark.read
-					.format(format)
-					.option("url", url)
-					.option("dbtable", dbtable)
-					.option("user", username)
-					.option("password", password)
-					.load()
-				return jdbcDF
+				try {
+					val jdbcDF_read : DataFrame = spark.read
+						.format(format)
+						.option("url", url)
+						.option("dbtable", dbtable)
+						.option("user", username)
+						.option("password", password)
+						.load()
+					info("DB2_Operations : " + "Read data from table " + dbtable + " from DB2")
+					return jdbcDF_read
+
+				} catch {
+					case e  : Throwable => error("DB2_Operations : " + "Error while reading data from table " + dbtable + " from DB2", e)
+					return spark.emptyDataFrame
+				}
 			}
 			case "write" => {
-				jdbcDF.write
-					.format(format)
-					.option("url", url)
-					.option("dbtable", dbtable)
-					.option("user", username)
-					.option("password", password)
-					.save()
-				return jdbcDF
+				try {
+					jdbcDF.write
+						.format(format)
+						.option("url", url)
+						.option("dbtable", dbtable)
+						.option("user", username)
+						.option("password", password)
+						.save()
+					info("DB2_Operations : " + "Written data to table " + dbtable + " in DB2")
+					return jdbcDF
+				} catch {
+					case e  : Throwable => error("DB2_Operations : " + "Error while writing data to table " + dbtable + " in DB2", e)
+					return spark.emptyDataFrame
+				}
 			}
 			case _ => {
 				return jdbcDF
